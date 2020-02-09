3829fc10b4a53daa93e0d714d8f51cbfb03c3504
Chris Mkolongo <chrismks23@gmail.com>
2020-02-09 13:10:11 +0200

    validation of input data

--- src/com/mkolongo/Main.java
+++ src/com/mkolongo/Main.java
@@ -7,8 +7,15 @@ public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        System.out.print("Please provide the size of logo(odd number): ");
+        System.out.println("Please provide the size of logo(odd number).");
+        System.out.print("Size(2 - 10000): ");
         int n = scanner.nextInt();
+
+        while (n <= 2 || n >= 10000 || n % 2 == 0) {
+            System.out.println("Invalid size!");
+            System.out.print("Size(2 - 10000): ");
+            n = scanner.nextInt();
+        }
 
         LogoMaker logoMaker = new MMLogo(n);
         System.out.println(logoMaker.print());
